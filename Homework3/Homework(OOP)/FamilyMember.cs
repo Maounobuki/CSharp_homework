2aa4cb871c80990a5f52b5cdd6cc062361125fa0
Maounobuki <116195152+Maounobuki@users.noreply.github.com>
2023-10-17 15:20:29 +0300

    Homework OOP done

---
+++ Homework3/Homework(OOP)/FamilyMember.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_OOP_
+{
+    internal class FamilyMember
+    {
+        public string Name;
+        public DateTime Birthday;
+        public FamilyMember Spouse = null;
+        public FamilyMember Father = null;
+        public FamilyMember Mother = null;
+        public FamilyMember[] Children = null;
+      
+
+        public FamilyMember(string name, DateTime birthday)
+        {
+            Name = name;
+            Birthday = birthday;
+        }
+      
+        void Print()
+        {
+            Console.WriteLine($"Имя: {Name}, дата рождения = {Birthday.ToString("dd.mm.yyyy")} ");
+        }
+
+        public void AddFamilyInfo(FamilyMember spouse, FamilyMember father, FamilyMember mother, params FamilyMember[] children)
+        {
+            Spouse = spouse;
+            Father = father;
+            Mother = mother;
+            Children = children;
+        }
+
+        public void PrintFamilyInfo()
+        {
+            if ( Spouse != null )
+            {
+                Console.Write($"Супруг(а): ");
+                Spouse.Print();
+            }
+            if ( Father != null )
+            {
+                Console.Write($"Отец: ");
+                Father.Print();
+            }
+            if( Mother != null )
+            {
+                Console.Write($"Мать: ");
+                Mother.Print();
+            }
+            if( Children != null )
+            {
+                Console.WriteLine($"Дети: ");
+                foreach ( var child in Children )
+                {
+                    child.Print();
+                }
+            }
+        }
+    }
+
+  
+}
---
+++ Homework3/Homework(OOP)/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Homework_OOP_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var husband = new FamilyMember("Олег", DateTime.Parse("20.08.1992"));
+            var wife = new FamilyMember("Мария", DateTime.Parse("06.06.1993"));
+            var wiwesfather = new FamilyMember("Сергей", DateTime.Parse("25.03.1976"));
+            var wiwesmother = new FamilyMember("Елена", DateTime.Parse("29.02.1980"));
+            var husbandsfather = new FamilyMember("Андрей", DateTime.Parse("21.09.1975"));
+            var husbandsmother = new FamilyMember("Нина", DateTime.Parse("30.05.1978"));
+            var son = new FamilyMember("Евгений", DateTime.Parse("17.01.2014"));
+            var dauther = new FamilyMember("Ангелина", DateTime.Parse("26.07.2018"));
+
+            husband.AddFamilyInfo(wife, husbandsfather, husbandsmother, son, dauther);
+            wife.AddFamilyInfo(husband, wiwesfather, wiwesmother, son, dauther);
+            husband.PrintFamilyInfo();
+            wife.PrintFamilyInfo();
+            Console.ReadLine();
+        }
+
+        
+    }
+}
