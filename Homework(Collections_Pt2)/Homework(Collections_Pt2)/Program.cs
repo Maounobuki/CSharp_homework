803aecbda785e7e315e16a58115aafb5a4eec430
Maounobuki <116195152+Maounobuki@users.noreply.github.com>
2023-10-29 21:37:50 +0300

    Homework Collections Pt2 done

---
+++ Homework(Collections_Pt2)/Homework(Collections_Pt2)/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+namespace Homework_Collections_Pt2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
+            Console.Write("Введите целевое число:");
+            int target = int.Parse(Console.ReadLine());
+
+            DefineSummand(numbers, target);
+
+
+            static bool DefineSummand(int[] numbers, int target)
+            {
+                for (int i = 1; i < numbers.Length - 1; i++)
+                {
+                    int left = i - 1;
+                    int right = i + 1;
+                    int result;
+
+                    result = numbers[left] + numbers[i] + numbers[right];
+                    if (result == target)
+                    {
+                        Console.WriteLine($"Искомое число {target} состоит из {numbers[left]} + {numbers[i]} + {numbers[right]}");
+                        return true;
+                    }
+                    else
+                    {
+                        left++;
+                        right++;
+                    }
+                }
+                Console.WriteLine("Подходящих чисел не найдено");
+                return false;
+            }
+        }
+    }
+    }
