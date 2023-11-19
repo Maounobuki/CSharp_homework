80f5b60abbbfca8036e6c03ce5f5f5f41c3780e9
Maounobuki <116195152+Maounobuki@users.noreply.github.com>
2023-11-19 23:51:54 +0300

    Homework Exeptions done

---
+++ Homework(exeptions)/Homework_Exeptions/CalculatorDivideByZeroException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Exeptions
+{
+   public class CalculatorDivideByZeroException : CalculatorExeption
+    {
+        public CalculatorDivideByZeroException()
+        {
+        
+        }
+        public CalculatorDivideByZeroException(string error) : base(error)
+        {
+
+        }
+        
+    }
+
+}
