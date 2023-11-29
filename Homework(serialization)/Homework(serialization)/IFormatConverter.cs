8eeffc73185d3505d94dafd680ccb25abe7eac44
Maounobuki <116195152+Maounobuki@users.noreply.github.com>
2023-11-29 23:43:14 +0300

    Homework Serialization done

---
+++ Homework(serialization)/Homework(serialization)/IFormatConverter.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.Json;
+using System.Xml;
+
+namespace Homework_serialization_
+{
+    internal interface IFormatConverter
+    {
+        static string ConvertJsonToXml(string jsonData)
+        {
+           
+            JsonDocument jsonDoc = JsonDocument.Parse(jsonData);
+
+            
+            var xmlDoc = new XmlDocument();
+            var rootElement = xmlDoc.CreateElement("root");
+            xmlDoc.AppendChild(rootElement);
+
+            
+            ParseJson(xmlDoc, rootElement, jsonDoc.RootElement);
+
+            
+            return xmlDoc.OuterXml;
+        }
+
+        static void ParseJson(XmlDocument xmlDoc, XmlNode parentNode, JsonElement jsonElement)
+        {
+            switch (jsonElement.ValueKind)
+            {
+                case JsonValueKind.Object:
+                    foreach (var property in jsonElement.EnumerateObject())
+                    {
+                        var subElement = xmlDoc.CreateElement(property.Name);
+                        parentNode.AppendChild(subElement);
+                        ParseJson(xmlDoc, subElement, property.Value);
+                    }
+                    break;
+                case JsonValueKind.Array:
+                    int index = 1;
+                    foreach (var item in jsonElement.EnumerateArray())
+                    {
+                        var subElement = xmlDoc.CreateElement("item");
+                        parentNode.AppendChild(subElement);
+                        ParseJson(xmlDoc, subElement, item);
+                        index++;
+                    }
+                    break;
+                default:
+                    parentNode.InnerText = jsonElement.ToString();
+                    break;
+            }
+        }
+    }
+}
---
+++ Homework(serialization)/Homework(serialization)/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Homework_serialization_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+          StreamReader json = new StreamReader("E:\\C#\\CSharp\\Homework(serialization)\\Homework(serialization)\\sample4.json");
+          string jsonString = json.ReadToEnd();
+         
+
+                
+                string xmlData = IFormatConverter.ConvertJsonToXml(jsonString);
+
+               
+                Console.WriteLine(xmlData);
+            }
+
+
+        }
+    }
+
