0fad9ea0a537e6808b76418c23401acbe32ad8df
Maounobuki <116195152+Maounobuki@users.noreply.github.com>
2023-12-17 18:51:43 +0300

    EF homework done

---
+++ Homework(Entity_Databases)/Homework(Entity_Databases)/Abstracts/IMessageSouce.cs
@@ -0,0 +1,17 @@
+﻿using Homework_Entity_Databases_.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Entity_Databases_
+{
+    public interface IMessageSource
+    {
+        Task SendAsync(NetMessage message , IPEndPoint ep);
+
+        NetMessage Receive(ref IPEndPoint ep);
+    }
+}
